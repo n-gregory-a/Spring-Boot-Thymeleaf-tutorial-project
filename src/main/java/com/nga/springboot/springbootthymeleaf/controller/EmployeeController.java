3a946fd94e0bbc3dd633cedd611fe3b72a986a8b
Grigory <ovonely@gmail.com>
2020-02-04 22:11:34 +0700

    Spring Boot - Thymeleaf - CRUD Database:   - modifying EmployeeController to get data from db

--- src/main/java/com/nga/springboot/springbootthymeleaf/controller/EmployeeController.java
+++ src/main/java/com/nga/springboot/springbootthymeleaf/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.nga.springboot.springbootthymeleaf.controller;
 
 import com.nga.springboot.springbootthymeleaf.entity.Employee;
+import com.nga.springboot.springbootthymeleaf.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,29 +16,17 @@ import java.util.List;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    // load employee data
-    private List<Employee> employees;
+    private EmployeeService employeeService;
 
-    @PostConstruct
-    private void loadData() {
-
-        // create employees
-        Employee employee1 = new Employee(1, "Leslie", "Andrews", "leslie@gmail.com");
-        Employee employee2 = new Employee(2, "Emma", "Baumgarten", "emma@gmail.com");
-        Employee employee3 = new Employee(3, "Avani", "Gupta", "avani@gmail.com");
-
-        // create the list
-        employees = new ArrayList<>();
-
-        // add to the list
-        employees.add(employee1);
-        employees.add(employee2);
-        employees.add(employee3);
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     // add mapping for "/list"
     @GetMapping("/list")
     public String listEmployees (Model model) {
+
+        List<Employee> employees = employeeService.findAll();
 
         // add tj the spring model
         model.addAttribute("employees", employees);
