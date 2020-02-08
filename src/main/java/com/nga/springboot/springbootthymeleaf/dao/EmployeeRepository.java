ce32a2a272c6b600e0e7138d5850d8bb551130f7
Grigory <ovonely@gmail.com>
2020-02-08 18:49:34 +0700

    Spring Boot - Thymeleaf - CRUD Database:   - adding ordering by last name

--- src/main/java/com/nga/springboot/springbootthymeleaf/dao/EmployeeRepository.java
+++ src/main/java/com/nga/springboot/springbootthymeleaf/dao/EmployeeRepository.java
@@ -3,6 +3,11 @@ package com.nga.springboot.springbootthymeleaf.dao;
 import com.nga.springboot.springbootthymeleaf.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository  extends JpaRepository<Employee, Integer> {
+
+    // method to sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
     
 }
